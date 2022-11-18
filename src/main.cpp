d5ba2c008878c130c29107cd3256999f644930b1
hangocdinhnew <dinhndrwnew@gmail.com>
2022-11-18 21:32:04 +0700

    FUCK

--- src/main.cpp
+++ src/main.cpp
@@ -202,6 +202,7 @@ int main()
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
         if (show_demo_window)
             ImGui::ShowDemoWindow(&show_demo_window);
+
             // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
             static float f = 0.0f;
