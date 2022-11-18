adb704f3b5a22bf7af516ebd9b7af099a142eece
hangocdinhnew <dinhndrwnew@gmail.com>
2022-11-18 21:13:46 +0700

    EASTER EGG?

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "MyApp", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
