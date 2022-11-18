43840314510c9cdb1f54b0b2415da024c8e75a11
hangocdinhnew <dinhndrwnew@gmail.com>
2022-11-18 21:29:12 +0700

    NICE

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ int main()
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
-    
+
     // Decide GL+GLSL versions
 #if defined(IMGUI_IMPL_OPENGL_ES2)
     // GL ES 2.0 + GLSL 100
@@ -202,8 +202,7 @@ int main()
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
         if (show_demo_window)
             ImGui::ShowDemoWindow(&show_demo_window);
-
-                // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
             static float f = 0.0f;
             static int counter = 0;
