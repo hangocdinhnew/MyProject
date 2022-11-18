b4ace3c3fce38f50015e11e78374c294515a391a
hangocdinhnew <dinhndrwnew@gmail.com>
2022-11-18 19:31:18 +0700

    Unnecessary change

--- src/Config/MPConfig.h
+++ src/Config/MPConfig.h
@@ -2,3 +2,6 @@
 #define MyProject_VERSION_MINOR 0
 
 #include <iostream>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <string.h>
--- src/main.cpp
+++ src/main.cpp
@@ -96,7 +96,7 @@ int main(int, char**)
     bool show_demo_window = true;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    
+
     // Main loop
     while (!glfwWindowShouldClose(window))
     {
