e6f214f2cde1e6b8e91a672d942e307aa1569255
hangocdinhnew <dinhndrwnew@gmail.com>
2022-12-18 13:02:56 +0700

    configure

--- configure.sh
+++ configure.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
 cmake -B build -G "Ninja" -DCMAKE_BUILD_TYPE="Release" -DUSE_DOXYGEN="True" -DUSE_CPPCHECK="True";
+# shellcheck disable=SC2164
 cd build;
 ninja;
+# shellcheck disable=SC2103
 cd ..;
 echo ""
 echo "Building done!"
