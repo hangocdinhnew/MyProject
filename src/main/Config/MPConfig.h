171f6cdecffa42d20ae923f8eb9c30bf97e0df2e
hangocdinhnew <dinhndrwnew@gmail.com>
2022-12-24 15:39:04 +0700

    MPConfig.h

--- src/main/Config/MPConfig.h
+++ src/main/Config/MPConfig.h
@@ -10,7 +10,11 @@
 #include <GLFW/glfw3.h>
 #include <imgui.h>
 
-#include <opencv2/opencv.hpp>
+/* #undef INCLUDE_OPENCV */
+
+#ifdef INCLUDE_OPENCV
+    #include <opencv2/opencv.hpp>
+#endif
 
 #include "../Sound/SoundDevice.h"
 #include "../Sound/SoundEffectsPlayer.h"
