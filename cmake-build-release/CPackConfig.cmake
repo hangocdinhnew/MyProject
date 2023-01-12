544f73f4adee55ab9763ea4ce28248ff9e18397f
hangocdinhnew <dinhndrwnew@gmail.com>
2023-01-12 12:27:41 +0700

    fix: 🐛 Remove support for Discord RPC
    This update remove support for Discord RPC because discord end life of
    support

---
+++ cmake-build-release/CPackConfig.cmake
@@ -0,0 +1,69 @@
+# This file will be configured to contain variables for CPack. These variables
+# should be set in the CMake list file of the project before CPack module is
+# included. The list of available CPACK_xxx variables and their associated
+# documentation may be obtained using
+#  cpack --help-variable-list
+#
+# Some variables are common to all generators (e.g. CPACK_PACKAGE_NAME)
+# and some are specific to a generator
+# (e.g. CPACK_NSIS_EXTRA_INSTALL_COMMANDS). The generator specific variables
+# usually begin with CPACK_<GENNAME>_xxxx.
+
+
+set(CPACK_BUILD_SOURCE_DIRS "/Users/dinhndrwmacbook/DEV/C++/MyProject;/Users/dinhndrwmacbook/DEV/C++/MyProject/cmake-build-release")
+set(CPACK_CMAKE_GENERATOR "Ninja")
+set(CPACK_COMPONENT_UNSPECIFIED_HIDDEN "TRUE")
+set(CPACK_COMPONENT_UNSPECIFIED_REQUIRED "TRUE")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_FILE "/Applications/CLion.app/Contents/bin/cmake/mac/share/cmake-3.24/Templates/CPack.GenericDescription.txt")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_SUMMARY "MyProject built using CMake")
+set(CPACK_DMG_SLA_USE_RESOURCE_FILE_LICENSE "ON")
+set(CPACK_GENERATOR "DRAGNDROP")
+set(CPACK_INSTALL_CMAKE_PROJECTS "/Users/dinhndrwmacbook/DEV/C++/MyProject/cmake-build-release;MyProject;ALL;/")
+set(CPACK_INSTALL_PREFIX "/usr/local")
+set(CPACK_MODULE_PATH "/Users/dinhndrwmacbook/DEV/vcpkg/installed/arm64-osx/share/Catch2")
+set(CPACK_NSIS_DISPLAY_NAME "MyProject 1.9.4")
+set(CPACK_NSIS_INSTALLER_ICON_CODE "")
+set(CPACK_NSIS_INSTALLER_MUI_ICON_CODE "")
+set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES")
+set(CPACK_NSIS_PACKAGE_NAME "MyProject 1.9.4")
+set(CPACK_NSIS_UNINSTALL_NAME "Uninstall")
+set(CPACK_OSX_SYSROOT "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.0.sdk")
+set(CPACK_OUTPUT_CONFIG_FILE "/Users/dinhndrwmacbook/DEV/C++/MyProject/cmake-build-release/CPackConfig.cmake")
+set(CPACK_PACKAGE_DEFAULT_LOCATION "/")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "/Applications/CLion.app/Contents/bin/cmake/mac/share/cmake-3.24/Templates/CPack.GenericDescription.txt")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "The Greatest App Ever")
+set(CPACK_PACKAGE_FILE_NAME "MyProject-1.9.4-Darwin")
+set(CPACK_PACKAGE_ICON "/Users/dinhndrwmacbook/DEV/C++/MyProject/res/ico.icns")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "MyProject 1.9.4")
+set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "MyProject 1.9.4")
+set(CPACK_PACKAGE_NAME "MyProject")
+set(CPACK_PACKAGE_RELOCATABLE "true")
+set(CPACK_PACKAGE_VENDOR "Humanity")
+set(CPACK_PACKAGE_VERSION "1.9.4")
+set(CPACK_PACKAGE_VERSION_MAJOR "1")
+set(CPACK_PACKAGE_VERSION_MINOR "9")
+set(CPACK_PACKAGE_VERSION_PATCH "4")
+set(CPACK_RESOURCE_FILE_LICENSE "/Users/dinhndrwmacbook/DEV/C++/MyProject/LICENSE")
+set(CPACK_RESOURCE_FILE_README "/Applications/CLion.app/Contents/bin/cmake/mac/share/cmake-3.24/Templates/CPack.GenericDescription.txt")
+set(CPACK_RESOURCE_FILE_WELCOME "/Applications/CLion.app/Contents/bin/cmake/mac/share/cmake-3.24/Templates/CPack.GenericWelcome.txt")
+set(CPACK_SET_DESTDIR "OFF")
+set(CPACK_SOURCE_GENERATOR "TBZ2;TGZ;TXZ;TZ")
+set(CPACK_SOURCE_OUTPUT_CONFIG_FILE "/Users/dinhndrwmacbook/DEV/C++/MyProject/cmake-build-release/CPackSourceConfig.cmake")
+set(CPACK_SOURCE_RPM "OFF")
+set(CPACK_SOURCE_TBZ2 "ON")
+set(CPACK_SOURCE_TGZ "ON")
+set(CPACK_SOURCE_TXZ "ON")
+set(CPACK_SOURCE_TZ "ON")
+set(CPACK_SOURCE_ZIP "OFF")
+set(CPACK_SYSTEM_NAME "Darwin")
+set(CPACK_THREADS "1")
+set(CPACK_TOPLEVEL_TAG "Darwin")
+set(CPACK_WIX_SIZEOF_VOID_P "8")
+
+if(NOT CPACK_PROPERTIES_FILE)
+  set(CPACK_PROPERTIES_FILE "/Users/dinhndrwmacbook/DEV/C++/MyProject/cmake-build-release/CPackProperties.cmake")
+endif()
+
+if(EXISTS ${CPACK_PROPERTIES_FILE})
+  include(${CPACK_PROPERTIES_FILE})
+endif()
--- src/main/Discord/Discord.cpp
+++
@@ -1,6 +0,0 @@
-#include "Discord.h"
-
-void Discord::Initialize()
-{
-    DiscordEventHandlers handlerpc;
-}
